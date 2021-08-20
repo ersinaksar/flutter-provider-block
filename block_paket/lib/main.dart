03d6db2c76dbd481334ba65e486547b713d7d73e
ersinaksar <ersinaksar100@gmail.com>
2021-08-20 16:29:22 +0300

    bloc cubit kullanımı ile tema değiştirme block builder bloc provider

--- block_paket/lib/main.dart
+++ block_paket/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:block_paket/counter_bloc/counter_block.dart';
 import 'package:block_paket/counter_bloc/counter_event.dart';
 import 'package:block_paket/counter_bloc/counter_state.dart';
+import 'package:block_paket/theme_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,13 +12,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BlocProvider(
-          create: (context) => CounterBlock(), child: MyHomePage()),
+    return BlocProvider(
+      //tüm ağaca bu değeri verdik material app artık bu değeri biliyor
+      create: (context) => ThemeCubit(),
+      child: BlocBuilder<ThemeCubit, ThemeData>(builder: (context, tema) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: tema,
+          home: BlocProvider(
+              create: (context) => CounterBlock(), child: MyHomePage()),
+        );
+      }),
     );
   }
 }
@@ -91,6 +96,14 @@ class MyActions extends StatelessWidget {
           },
           child: Icon(Icons.remove),
           tooltip: "Azalt",
+        ),
+        FloatingActionButton(
+          heroTag: "3",
+          onPressed: () {
+            context.read<ThemeCubit>().temaDegistir();
+          },
+          child: Icon(Icons.remove),
+          tooltip: "Tema DEğiştir",
         ),
       ],
     );
